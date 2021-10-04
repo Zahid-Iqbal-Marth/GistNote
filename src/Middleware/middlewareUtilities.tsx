113bd37e7cbbedda95c92682d5b99a51aaae3603
Zahid-Iqbal-Marth <zi090545@gmail.com>
2021-10-04 11:33:54 +0500

    second commit

--- src/Middleware/middlewareUtilities.tsx
+++ src/Middleware/middlewareUtilities.tsx
@@ -1,5 +1,5 @@
 export const returnAPI : (params: string) => string = (params : string) => `https://api.github.com/${params}`
-export const AUTH_TOKEN = ' token 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';
+export const AUTH_TOKEN = ' token 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';
 export const returnHeaders :  (method?: string, body?: string, headers?: Headers) => {
         method: string;
         headers: Headers;
