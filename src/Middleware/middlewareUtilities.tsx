c30e1341fdf538d883e1022e2a719946d09f6933
Zahid-Iqbal-Marth <zi090545@gmail.com>
2022-02-02 12:25:32 +0500

    ready-for-production

--- src/Middleware/middlewareUtilities.tsx
+++ src/Middleware/middlewareUtilities.tsx
@@ -1,15 +1,20 @@
-export const returnAPI : (params: string) => string = (params : string) => `https://api.github.com/${params}`
-export const AUTH_TOKEN = ' token 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';
-export const returnHeaders :  (method?: string, body?: string, headers?: Headers) => {
-        method: string;
-        headers: Headers;
-        body: string | null;
-    }
-    = ( method = 'GET', 
-        body = '',
-        headers = new Headers({'Authorization': AUTH_TOKEN})) => (
-    { 
-        method, 
-        headers,
-        body : method === 'GET' ? null : body
-  })
+export const returnAPI: (params: string) => string = (params: string) =>
+  `https://api.github.com/${params}`;
+export const AUTH_TOKEN = " token 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";
+export const returnHeaders: (
+  method?: string,
+  body?: string,
+  headers?: Headers
+) => {
+  method: string;
+  headers: Headers;
+  body: string | null;
+} = (
+  method = "GET",
+  body = "",
+  headers = new Headers({ Authorization: AUTH_TOKEN })
+) => ({
+  method,
+  headers,
+  body: method === "GET" ? null : body,
+});
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,184 +1,187 @@
-import React, {useState, useCallback, ChangeEvent} from "react"
+import React, { useState, useCallback, ChangeEvent } from "react";
 import ProfilePicture from "../ProfilePicutre/ProfilePicture";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router";
 import { useSelector } from "react-redux";
 import { useAppDispatch } from "../../hooks";
-import {getAuthData} from "../../Selectors/ownerSelectors"
+import { getAuthData } from "../../Selectors/ownerSelectors";
 import { SearchByType, setError, setSearchBy } from "../../Slices/gistSlice";
 import { fetchGistNotesAPI } from "../../Middleware/gistMiddleware";
 import { setApiCallState } from "../../Slices/gistSlice";
 import { loginAPI } from "../../Middleware/ownerMiddleware";
 import { login, OwnerType } from "../../Slices/ownerSlice";
-import logo from "../../media/emumba-logo.png"
+import logo from "../../media/gistnotes.png";
 import { SearchRounded } from "@material-ui/icons";
-import "./Navbar.css"
+import "./Navbar.css";
 
-const Navbar : (props: object) => JSX.Element = (props: object) => {
-    //selectorzs
-    const {
-        ownerName : loggedInUserName,
-        dp : loggedInUserDP
-    } : OwnerType = useSelector(getAuthData);
+const Navbar: (props: object) => JSX.Element = (props: object) => {
+  //selectorzs
+  const { ownerName: loggedInUserName, dp: loggedInUserDP }: OwnerType =
+    useSelector(getAuthData);
 
-    //defining hooks
-    const dispatch = useAppDispatch()
-    const history = useHistory()
+  //defining hooks
+  const dispatch = useAppDispatch();
+  const history = useHistory();
 
-    //state
-    const [profileMenuTogge, setProfileMenuToggle] = useState<boolean>(false);
-    const searchType : string = 'id';
-    const [searchBy, setSearch] = useState<SearchByType>({
-        type : searchType,
-        content : ''
-    })
+  //state
+  const [profileMenuTogge, setProfileMenuToggle] = useState<boolean>(false);
+  const searchType: string = "id";
+  const [searchBy, setSearch] = useState<SearchByType>({
+    type: searchType,
+    content: "",
+  });
 
-    //event handlers
-    const handleError : (error: string) => void = useCallback((error : string) => {
-        dispatch(setApiCallState('idle')) 
-        dispatch(setError(error)) 
-    },[setApiCallState])
+  //event handlers
+  const handleError: (error: string) => void = useCallback(
+    (error: string) => {
+      dispatch(setApiCallState("idle"));
+      dispatch(setError(error));
+    },
+    [setApiCallState]
+  );
 
-    const handlesearch : () => void = useCallback(() => {
-        if (searchBy.content){
-        dispatch(setSearchBy({
-            type : searchType,
-            content : searchBy.content
-        }))
-        if (searchBy.type === 'id'){
-            dispatch(setApiCallState('loading')) 
-            dispatch(fetchGistNotesAPI({
-                handleError,
-                gistID : searchBy.content,
-                isLoggedIn : loggedInUserName ? true : false
-            }))
-            }
-        }
-    }, [searchBy])
-
-    const handleSearchByChange : (event: ChangeEvent<HTMLInputElement>) => void = useCallback((event: ChangeEvent<HTMLInputElement>) => setSearch({
-        type : searchType,
-        content : event.target.value
-    }), [setSearch])
-
-    const handleLogin : () => void = useCallback(() => {
-        dispatch(loginAPI("Zahid-Iqbal-Marth"))
-    }, [])
-
-    const handleProfileMenu : () => void = useCallback(() => {
-            setProfileMenuToggle(!profileMenuTogge)
-    }, [profileMenuTogge])
-
-    const handleSignOut : () => void = useCallback(() => {
-        handleProfileMenu()
-        dispatch(login(""))
-    }, [handleProfileMenu])
-
-    const handleLogoClick : () => void = useCallback(() => {
-        setSearch({
-            type : searchType,
-            content : ''
+  const handlesearch: () => void = useCallback(() => {
+    if (searchBy.content) {
+      dispatch(
+        setSearchBy({
+          type: searchType,
+          content: searchBy.content,
         })
-        dispatch(setSearchBy({
-            type : searchType,
-            content : ''
-        }))
-    },[setSearch] )
+      );
+      if (searchBy.type === "id") {
+        dispatch(setApiCallState("loading"));
+        dispatch(
+          fetchGistNotesAPI({
+            handleError,
+            gistID: searchBy.content,
+            isLoggedIn: loggedInUserName ? true : false,
+          })
+        );
+      }
+    }
+  }, [searchBy]);
 
-    //UI snipits
-    const loginButton : JSX.Element = (
-        <section  id = "login">
-            <button type = "button" 
-                    className="navbar-login-btn"
-                    onClick={handleLogin}>
-                    Login
-            </button>
-        </section>
-    )
-    const dpButton : JSX.Element = (
-            <button className="navbar-dp-btn"
-                    type="button"
-                    onClick={handleProfileMenu}>
-                <section  id = "dp" className="navbar-dp">
-                    <ProfilePicture  dp={loggedInUserDP}/>
-                </section>
-            </button>
-    )
-    const dpMenu : JSX.Element =  (
-                
-        <section className="navbar-profile-menu">
-            <section className="navbar-profile-menu-content" 
-                onMouseLeave={handleProfileMenu}
-                >
-                <p>
-                    Signed In as {loggedInUserName}
-                </p>
-                <hr />
-                <Link to={`/profile/${loggedInUserName}`} className="navbar-link-decor">
-                    <button type="button" onClick={handleProfileMenu}>
-                        Your gists
-                    </button>
-                </Link>
+  const handleSearchByChange: (event: ChangeEvent<HTMLInputElement>) => void =
+    useCallback(
+      (event: ChangeEvent<HTMLInputElement>) =>
+        setSearch({
+          type: searchType,
+          content: event.target.value,
+        }),
+      [setSearch]
+    );
 
-                <button type="button" >
-                    Stared gists
-                </button>
-                <Link to={'/gistinsertion'} className="navbar-link-decor">
-                    <button type="button" onClick={handleProfileMenu}>
-                        Add New Gist
-                    </button>
-                </Link>
-                <hr />
-                <button type="button" onClick={handleProfileMenu}>
-                    <a  
-                        href={`https://github.com/${loggedInUserName}`}
-                        target="_blank">
-                        View GitHub Profile
-                    </a>
-                </button>
-                    <button type="button"
-                            onClick={handleSignOut}>
-                        Sign out
-                    </button>
+  const handleLogin: () => void = useCallback(() => {
+    dispatch(loginAPI("Zahid-Iqbal-Marth"));
+  }, []);
+
+  const handleProfileMenu: () => void = useCallback(() => {
+    setProfileMenuToggle(!profileMenuTogge);
+  }, [profileMenuTogge]);
+
+  const handleSignOut: () => void = useCallback(() => {
+    handleProfileMenu();
+    dispatch(login(""));
+  }, [handleProfileMenu]);
+
+  const handleLogoClick: () => void = useCallback(() => {
+    setSearch({
+      type: searchType,
+      content: "",
+    });
+    dispatch(
+      setSearchBy({
+        type: searchType,
+        content: "",
+      })
+    );
+  }, [setSearch]);
+
+  //UI snipits
+  const loginButton: JSX.Element = (
+    <section id="login">
+      <button type="button" className="navbar-login-btn" onClick={handleLogin}>
+        Login
+      </button>
+    </section>
+  );
+  const dpButton: JSX.Element = (
+    <button className="navbar-dp-btn" type="button" onClick={handleProfileMenu}>
+      <section id="dp" className="navbar-dp">
+        <ProfilePicture dp={loggedInUserDP} />
+      </section>
+    </button>
+  );
+  const dpMenu: JSX.Element = (
+    <section className="navbar-profile-menu">
+      <section
+        className="navbar-profile-menu-content"
+        onMouseLeave={handleProfileMenu}
+      >
+        <p>Signed In as {loggedInUserName}</p>
+        <hr />
+        <Link to={`/profile/${loggedInUserName}`} className="navbar-link-decor">
+          <button type="button" onClick={handleProfileMenu}>
+            Your gists
+          </button>
+        </Link>
+
+        <button type="button">Stared gists</button>
+        <Link to={"/gistinsertion"} className="navbar-link-decor">
+          <button type="button" onClick={handleProfileMenu}>
+            Add New Gist
+          </button>
+        </Link>
+        <hr />
+        <button type="button" onClick={handleProfileMenu}>
+          <a href={`https://github.com/${loggedInUserName}`} target="_blank">
+            View GitHub Profile
+          </a>
+        </button>
+        <button type="button" onClick={handleSignOut}>
+          Sign out
+        </button>
+      </section>
+    </section>
+  );
+  return (
+    <>
+      <nav>
+        <section className="navbar-nav-bar" id="navbar">
+          <section id="logo">
+            <Link to="/" className="navbar-link-decor">
+              <img
+                className="navbar-logo"
+                src={logo}
+                alt="logo"
+                onClick={handleLogoClick}
+              />
+            </Link>
+          </section>
+          <section className="navbar-left">
+            <section className="navbar-search-section" id="search-bar">
+              <input
+                className="navbar-search-bar"
+                type="text"
+                placeholder="Search Notes..."
+                value={searchBy.content}
+                onChange={handleSearchByChange}
+              />
+              <button
+                type="button"
+                className="navbar-search-btn"
+                onClick={handlesearch}
+              >
+                <SearchRounded className="navbar-search-icon" />
+              </button>
             </section>
+            <section>{loggedInUserName ? dpButton : loginButton}</section>
+          </section>
         </section>
-        
-    )
-    return (
-        <>
-        <nav>
-            <section className="navbar-nav-bar" id = "navbar">
-                <section id = "logo">
-                    <Link to="/" className="navbar-link-decor">
-                        <img className="navbar-logo" src={logo} alt="logo" onClick={handleLogoClick}/>
-                    </Link>
-                </section>
-                <section className="navbar-left">
-                    <section className="navbar-search-section" id ="search-bar">
-                        <input  className="navbar-search-bar" 
-                                type="text" 
-                                placeholder="Search Notes..." 
-                                value={searchBy.content}
-                                onChange={handleSearchByChange}/>
-                        <button type="button" 
-                                className="navbar-search-btn" 
-                                onClick={handlesearch}
-                                >
-                            <SearchRounded className="navbar-search-icon"/>
-                        </button>
-                    </section>
-                    <section>
-                        {loggedInUserName ?  dpButton : loginButton}
-                    </section>
-                </section>
-            </section>
-        </nav>
-        {
-            profileMenuTogge ? dpMenu : undefined
-        }
-        </>
-    )
-}
-
+      </nav>
+      {profileMenuTogge ? dpMenu : undefined}
+    </>
+  );
+};
 
 export default Navbar;
